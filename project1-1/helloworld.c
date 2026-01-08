878f1221cd85ccd24d63cb4f4f2f80b21ae7ff98
Alec <aleckrsek@gmail.com>
2026-01-08 15:03:31 -0800

    Finish hello world

--- project1-1/helloworld.c
+++ project1-1/helloworld.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
+    int x = 12;
+    float y = 3.14;
+    char s[] = "Hello, world!\n";
 
+    printf("x is %d, y is %f\n", x, y);
+    printf("%s", s);
+
+    for(int i = 0; i<5; i++){
+        printf("%d x 5 = %d\n", i, i*5);
+    }
 }
