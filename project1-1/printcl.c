1a64d6085e61a5380aab4b9d4d7cd2fb37b5d7c4
Alec Krsek <aleckrsek@gmail.com>
2026-01-11 21:19:57 -0800

    Implement printcl.c

--- project1-1/printcl.c
+++ project1-1/printcl.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("argc is %d\n", argc);
-    printf("argv[0] is \"%s\"\n", argv[0]);
     for(int i = 1; i < argc; i++){
-        printf()
+        printf("%d: %s\n", i, argv[i]);
     }
 }
