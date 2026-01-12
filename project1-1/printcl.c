6e312875b50a511311ed0f03c8a3db7dbec16071
Alec <aleckrsek@gmail.com>
2026-01-11 21:12:07 -0800

    Add laptop changes

---
+++ project1-1/printcl.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("argc is %d\n", argc);
+    printf("argv[0] is \"%s\"\n", argv[0]);
+    for(int i = 1; i < argc; i++){
+        printf()
+    }
+}
