5a38299864e9867a033a032758582a3c3e6ae030
Alec <aleckrsek@gmail.com>
2026-01-14 11:13:12 -0800

    return -1

--- week2/mymkdir.c
+++ week2/mymkdir.c
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[]){
     if(argc != 2){
         printf("usage: mymakedir dir\n");
+        return -1;
     } else {
         mkdir(argv[1], 0777);
         perror(argv[1]);
