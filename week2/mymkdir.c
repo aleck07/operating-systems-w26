2d369da32879544e678b2aaee62376ff16e5252d
Alec <aleckrsek@gmail.com>
2026-01-14 11:03:36 -0800

    Implement challenge 2.3

---
+++ week2/mymkdir.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[]){
+    if(argc != 2){
+        printf("usage: mymakedir dir\n");
+    } else {
+        mkdir(argv[1], 0777);
+        perror(argv[1]);
+    }
+
+}
