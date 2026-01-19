ad5ce01af93a438768ba6adcf5bd489f72232eb7
Alec Krsek <aleckrsek@gmail.com>
2026-01-18 19:31:33 -0800

    Add more error checking and conventions

--- project2/filescanner.c
+++ project2/filescanner.c
@@ -6,7 +6,7 @@
 
 // Creates full file path for file
 void buildPath(char *dest, char *dirPath, struct dirent *dir){
-    sprintf(dest, "%s/%s", dirPath, dir->d_name);
+    snprintf(dest, 256, "%s/%s", dirPath, dir->d_name);
 }
 
 ssize_t readDirectory(char *dirPath){
@@ -16,12 +16,23 @@ ssize_t readDirectory(char *dirPath){
     ssize_t byteTotal = 0;
     fd = opendir(dirPath);
 
+    // Will result in error if file can't open
+    if(fd == NULL) {
+        perror("opendir");
+        return 0;
+    }
+
     while((dir = readdir(fd)) != NULL){
         char fullPath[256];
         buildPath(fullPath, dirPath, dir);
         struct stat statbuf;
-        stat(fullPath, &statbuf);
-
+        
+        // Get the file information of the path, and error check it.
+        if(stat(fullPath, &statbuf) == -1) {
+            perror("stat");
+            continue;
+        }
+        
         // Ignores . and .. but also hidden files :( 
         if(dir->d_name[0] == '.'){
             continue;
@@ -30,8 +41,7 @@ ssize_t readDirectory(char *dirPath){
         // Checks to see if the file is a directory or file, then adds the byte total of the file or dives deeper
         if(S_ISREG(statbuf.st_mode)){
             byteTotal += statbuf.st_size;
-        }
-        if(S_ISDIR(statbuf.st_mode)){
+        } else if(S_ISDIR(statbuf.st_mode)){
             byteTotal += readDirectory(fullPath);
         }
     }
@@ -41,10 +51,10 @@ ssize_t readDirectory(char *dirPath){
 }
 
 int main(int argc, char *argv[]){
-
     if(argc != 2){
         fprintf(stderr, "usage: filescanner [directory]\n");
         return 1;
     }
     readDirectory(argv[1]);
+    return 0;
 }
