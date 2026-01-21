e289b840ec953cba26271995888f106974565d7f
Alec <aleckrsek@gmail.com>
2026-01-21 11:32:31 -0800

    Add questions

--- week3/tokenize.c
+++ week3/tokenize.c
@@ -13,3 +13,6 @@ int main(int argc, char *argv[])
         i += 1;
     } while ((token = strtok(NULL, " ")) != NULL);
 }
+
+// We pass the pointer the first time but not the second time to the string because when call it the first time, the function stores the pointer to the string and remembers where it placed the null terminators.
+// If we kept on passing the pointer, we would keep on getting the first token over and over forever.
