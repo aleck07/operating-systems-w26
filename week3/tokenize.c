513f654bef4c1e974fae6d553902af5faed9d231
Alec <aleckrsek@gmail.com>
2026-01-21 11:25:54 -0800

    Finish tokenize.c

--- week3/tokenize.c
+++ week3/tokenize.c
@@ -5,5 +5,11 @@ int main(int argc, char *argv[])
 {
     (void)argc; // stop compiler warnings about ununsed variables
 
-    printf("The first argument is: \"%s\"\n", argv[1]); // just to demo
+    char* token = strtok(argv[1], " ");
+    int i = 0;
+
+    if (token != NULL) do {
+        printf("%d: %s\n", i, token);      
+        i += 1;
+    } while ((token = strtok(NULL, " ")) != NULL);
 }
