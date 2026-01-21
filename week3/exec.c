314781ae275a088dcdfaad1526accda0b4d587af
Alec <aleckrsek@gmail.com>
2026-01-21 11:06:26 -0800

    Fix print out for exec

--- week3/exec.c
+++ week3/exec.c
@@ -5,15 +5,13 @@
 
 int main(void)
 {
-    printf("And...\n");
-
     pid_t pid = fork();
 
     if(pid == 0){
         execlp("ls", "ls", NULL);
     } else {
         printf("I'm the parent\n");
-        printf("We're done\n");
         wait(NULL);
     }
+    printf("We're done\n");
 }
