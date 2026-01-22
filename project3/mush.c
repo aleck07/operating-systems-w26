622b05a22a8db18b399d72d2b0efd0adcb7f86b1
Alec <aleckrsek@gmail.com>
2026-01-22 14:35:29 -0800

    Part 1

--- project3/mush.c
+++ project3/mush.c
@@ -4,6 +4,29 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(int argc, char* argv[]){
-    
+#define MAX_LINE 2048
+#define MAX_TOKENS 128
+
+int main(void){
+    while(1){
+        char line[MAX_LINE];
+        char *tokens[MAX_TOKENS] = {0};
+
+        printf("prompt> ");
+        fflush(stdout); // flushes output
+
+        fgets(line, sizeof line, stdin);
+        char *token = strtok(line, " \n");
+
+        int i = 0;
+        while (token != NULL) {
+            tokens[i++] = token;
+            token = strtok(NULL, " \n");
+        }
+
+        if (tokens[0] != NULL) {
+            execvp(tokens[0], tokens);
+            perror("Invalid command");
+        }
+    }
 }
