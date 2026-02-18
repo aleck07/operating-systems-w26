685370487e3a081f403a7329e1608b1920d65307
Alec Krsek <aleckrsek@gmail.com>
2026-02-18 11:09:28 -0800

    Add thread_id

--- week7/threads.c
+++ week7/threads.c
@@ -7,20 +7,24 @@
 
 void *run(void *args)
 {
-    int id = (int)(intptr_t)args;
-    printf("running thread %d!\n", id);
+    int *id = args;
+    printf("running thread %d!\n", *id);
     return NULL;
 }
 
 int main(void)
 {
     pthread_t thread[THREADS];
+    int thread_id[THREADS];
 
-    for (int i = 0; i < THREADS; i++) {
-        pthread_create(thread + i, NULL, run, (void *)(intptr_t)i);
+    for (int i = 0; i < THREADS; i++)
+    {
+        thread_id[i] = i;
+        pthread_create(thread + i, NULL, run, (void *)&thread_id[i]);
     }
-    
-    for (int i = 0; i < THREADS; i++) {
+
+    for (int i = 0; i < THREADS; i++)
+    {
         pthread_join(thread[i], NULL);
     }
 
